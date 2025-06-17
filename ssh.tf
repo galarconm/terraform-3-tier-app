494fb73a6476a9233859911996690885141d3980
galarconm <galarconmadrid@gmail.com>
2025-06-17 15:24:28 +0200

    Fix public key assignment in SSH key resource

--- ssh.tf
+++ ssh.tf
@@ -1,6 +1,7 @@
 resource "aws_key_pair" "ssh_key" {
     key_name   = "my_ssh_key"
-    public_key = file(var.public_key)
+    #public_key = file(var.public_key)
+    public_key = var.public_key
     tags = {
         Name = "MySSHKey2"
     }
